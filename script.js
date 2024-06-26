a6bffaa18b98678ca6091a3289847feeba365067
jurre36 <66078766+jurre36@users.noreply.github.com>
2024-06-26 12:04:19 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('open-url-button').addEventListener('click', function() {
+    window.location.href = 'itms-services://?action=download-manifest&url=https://github.com/jurre36/whitelist/raw/main/whitelist.plist';
+});
